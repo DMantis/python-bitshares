55328aaccd41275ff238ab3dd0e2cc658db9218c
jhtitor <john.titor@openmailbox.org>
2018-07-16 11:43:28 +0000

    Add wipe method to Configuration storage.

--- bitshares/storage.py
+++ bitshares/storage.py
@@ -377,6 +377,12 @@ class Configuration(DataDir):
         cursor.execute(*query)
         connection.commit()
 
+    def wipe(self):
+        """ Delete all keys from the configuration store
+        """
+        query = ("DELETE FROM %s " % (self.__tablename__), ())
+        self.sql_execute(query)
+
     def __iter__(self):
         return iter(self.items())
 
